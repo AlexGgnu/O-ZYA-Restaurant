ba5c2b39d57bfedb6723c44c59bbaef1f8f4fd38
deusyg <moulayyoussef.ghazi@gmail.com>
2026-04-03 17:56:46 +0200

    fin fonction test panier php

--- paniertest.php
+++ paniertest.php
@@ -32,6 +32,33 @@
                             </div>
                         </div>
                     </div>
+                    </section>
+                
+                    <section class="flex-1 w-full flex-col gap-20">
+                        <div class="form-card gap-24 max-w-full m-0">
+                            <h2 class="text-primary font-600">Résumé</h2>
 
-  </body>
+                        <form method="POST">
+                            <div class="flex-col gap-10">
+                                <label class="font-600" for="promo">Code promo</label>
+                                <input id="promo" name="code_promo" type="text" placeholder="Entrer un code promo"
+                                    value="<?php if (isset($_POST['code_promo'])){ echo $_POST['code_promo']; } ?>">
+                                <button class="btn btn-secondary" type="submit">Appliquer</button>
+                            </div>
+                        </form>
+                                                <form class="flex-col gap-10" action='https://www.plateforme-smc.fr/cybank/index.php' method='POST'>
+                            <input type='hidden' name='transaction' value='UINIQUE_TRANSACTION_ID'>
+                            <input type='hidden' name='montant' value='TOTAL_AMOUNT'>
+                            <input type='hidden' name='vendeur' value='MI-3_C'>
+                            <input type='hidden' name='retour' value='http://localhost/retour_paiement.php?session=s'>
+                            <input type='hidden' name='control' value='01c06955b2d4ad0ccdedd4aad0ab68bf'>
+                            <input class="btn btn-primary" type='submit' value="Valider et payer">
+                        </form>
+                    </div>
+                </section>
+            </div>
+        </main>
+        
+        <?php echo get_footer(true); ?>
+    </body>
 </html>
